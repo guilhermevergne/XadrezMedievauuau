7cc2bc42feee8384114491ee8e4d5163ff8df6af
guilhermevergne <51097229+guilhermevergne@users.noreply.github.com>
2019-11-28 14:20:13 -0300

    Começo telinha

---
+++ src/xadrezmedievauuau/Guardiao_ao.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xadrezmedievauuau;
+
+import java.io.FileNotFoundException;
+import static java.lang.Math.abs;
+import javafx.scene.layout.GridPane;
+
+/**
+ *
+ * @author Gean Almeida
+ */
+public class Guardiao_ao extends Piece_ece {
+    public Guardiao_ao(String path, int Hpmax, String nome, int player, Casas_asas pos, int width, int height){
+        super(path, Hpmax, nome, player, pos);
+        //setImage(new Image(path, width, height, true, true));
+        DMG = 10;
+    }
+    
+    
+    
+    @Override
+    boolean moving(GridPane p, Casas_asas[][] table ,int x ,int y) throws FileNotFoundException {
+        if(ismoveAble()){
+            if(canMove(p, table , x , y)){
+                pos.setPiece(null);
+                pos.setPiece(null);
+                p.getChildren().remove(this);
+                p.add(this,x,y);
+                table[x][y].setPiece(this);
+                pos = table[x][y];   
+                System.out.println("Deu bom");
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    boolean canMove(GridPane p, Casas_asas[][] table ,int x ,int y){
+        if(abs(x - this.pos.getX()) + abs(y - this.pos.getY()) <= 3){
+        return true;
+        }
+        return false;
+    }
+
+    @Override
+    boolean atack(GridPane p, Casas_asas[][] table ,int x ,int y) throws FileNotFoundException {
+        return true;
+    }
+
+    @Override
+    boolean poderzinho(Casas_asas target) throws FileNotFoundException {
+        return true;
+    }
+    
+    @Override
+    boolean canAttack(GridPane p, Casas_asas[][] table, int x, int y) throws FileNotFoundException {
+        return false;
+    }
+
+    @Override
+    boolean canSpell(GridPane p, Casas_asas[][] table, int x, int y) throws FileNotFoundException {
+        return false;
+    }
+}
---
+++ src/xadrezmedievauuau/Piece_ece.java
@@ -0,0 +1,82 @@
+package xadrezmedievauuau;
+
+import java.io.FileNotFoundException;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public abstract class Piece_ece extends ImageView{
+    protected int Hpmax, Hp, DMG;
+    protected Casas_asas pos;
+    protected boolean moveAble, atackAble, skillAble;
+    protected String nome;
+    protected int player;
+
+    public int getPlayer() {
+        return player;
+    }
+    protected String path;
+    
+    public Piece_ece(String path, int Hpmax, String nome, int player, Casas_asas pos) {
+        super(path);
+        this.Hpmax = Hpmax;
+        this.Hp = Hpmax;
+        this.moveAble = false;
+        this.atackAble = false;
+        this.skillAble = false;
+        this.nome = nome;
+        this.player = player;
+        this.pos = pos;
+    }
+    
+    
+    public int getHp() {
+        return Hp;
+    }
+
+    public void setHp(int Hp) {
+        this.Hp = Hp;
+    }
+
+    public Casas_asas getPos() {
+        return pos;
+    }
+
+    public void setPos(Casas_asas pos) {
+        this.pos = pos;
+    }
+
+    public boolean ismoveAble() {
+        return moveAble;
+    }
+
+    public void setmoveAble(boolean canmove) {
+        this.moveAble = canmove;
+    }
+
+    public boolean isatackAble() {
+        return atackAble;
+    }
+
+    public void setatackAble(boolean canatack) {
+        this.atackAble = canatack;
+    }
+
+    public boolean isskillAble() {
+        return skillAble;
+    }
+
+    public void setskillAble(boolean canskill) {
+        this.skillAble = canskill;
+    }
+    
+    abstract boolean moving(GridPane p,Casas_asas[][] table ,int x ,int y)throws FileNotFoundException;
+    abstract boolean atack(GridPane p,Casas_asas[][] table ,int x ,int y)throws FileNotFoundException;
+    abstract boolean poderzinho(Casas_asas target)throws FileNotFoundException;
+    abstract boolean canMove(GridPane p, Casas_asas[][] table ,int x ,int y)  throws FileNotFoundException;
+    abstract boolean canAttack(GridPane p, Casas_asas[][] table ,int x ,int y)  throws FileNotFoundException;
+    abstract boolean canSpell(GridPane p, Casas_asas[][] table ,int x ,int y)  throws FileNotFoundException;
+    
+    
+}
